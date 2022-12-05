d372439f15136da77cb143a3fed48b5e7ea1bdf4
abdoachhoubi <aachhoub@gmail.com>
2022-12-05 17:34:05 +0100

    Added containers to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 import Head from "next/head";
+import { Header, Main, Footer } from "../containers";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>init</title>
+        <title>init - acronym finder</title>
         <meta
           name="description"
           content="Acronym finder, it finds meanings of all acronyms including WTF? -> Where's The Food?"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
