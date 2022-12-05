f7a366643fb15dbd854fcf52800a6ab092d77379
abdoachhoubi <aachhoub@gmail.com>
2022-12-05 18:03:08 +0100

    Added Nav to Header and linked styles

--- containers/Header/Header.jsx
+++ containers/Header/Header.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Nav } from "../../components";
+import { HomeContext } from "../../contexts";
 
 const Header = () => {
+  const { width } = useContext(HomeContext);
+
   return (
     <header>
+      <Nav width={width} />
       <h1>Header</h1>
     </header>
   );
