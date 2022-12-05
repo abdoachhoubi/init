0ec593d190aa199aa9809e54efb2ba09010ee488
abdoachhoubi <aachhoub@gmail.com>
2022-12-05 16:01:02 +0100

    Added pages and styles

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>init</title>
+        <meta
+          name="description"
+          content="Acronym finder, it finds meanings of all acronyms including WTF? -> Where's The Food?"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
