9e1235ca0817c88ba777c440b158a346b9ca0c95
abdoachhoubi <aachhoub@gmail.com>
2022-12-05 18:03:37 +0100

    Added HomeContext

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,32 @@
+import React, { useState, useEffect, createContext } from "react";
 import Head from "next/head";
 import { Header, Main, Footer } from "../containers";
 
+export const HomeContext = createContext();
+
 export default function Home() {
+  const [size, setSize] = useState(0);
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    window.addEventListener("resize", () => setSize(window.innerWidth));
+    setWidth(window.innerWidth);
+  }, [size]);
+
   return (
-    <div>
-      <Head>
-        <title>init - acronym finder</title>
-        <meta
-          name="description"
-          content="Acronym finder, it finds meanings of all acronyms including WTF? -> Where's The Food?"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <HomeContext.Provider value={{ width: width }}>
+      <div>
+        <Head>
+          <title>init - acronym finder</title>
+          <meta
+            name="description"
+            content="Acronym finder, it finds meanings of all acronyms including WTF? -> Where's The Food?"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </HomeContext.Provider>
   );
 }
